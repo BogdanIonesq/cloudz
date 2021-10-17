948b13dd8cfadc77104f6af92660b6e2041dd76d
Bogdan Ionescu <14040788+BogdanIonesq@users.noreply.github.com>
2021-10-17 16:21:29 +0300

    read encryption key from redis

--- app/go.mod
+++ app/go.mod
@@ -2,6 +2,13 @@ module app
 
 go 1.17
 
-require golang.org/x/crypto v0.0.0-20210921155107-089bfa567519
+require (
+	github.com/go-redis/redis/v8 v8.11.4
+	golang.org/x/crypto v0.0.0-20210921155107-089bfa567519
+)
 
-require golang.org/x/sys v0.0.0-20210615035016-665e8c7367d1 // indirect
+require (
+	github.com/cespare/xxhash/v2 v2.1.2 // indirect
+	github.com/dgryski/go-rendezvous v0.0.0-20200823014737-9f7001d12a5f // indirect
+	golang.org/x/sys v0.0.0-20210615035016-665e8c7367d1 // indirect
+)
--- app/main.go
+++ app/main.go
@@ -2,25 +2,25 @@ package main
 
 import (
 	"bytes"
+	"context"
 	"crypto/rand"
 	"encoding/base64"
 	"encoding/json"
+	"github.com/go-redis/redis/v8"
 	"golang.org/x/crypto/chacha20"
 	"io/ioutil"
 	"log"
 	"net/http"
 )
 
+const NONCE_SIZE = 24
+var ctx = context.Background()
 var KEY []byte
+var redisClient *redis.Client
 
-type EncryptRequest struct {
+type Payload struct {
 	// base64 encoded string of data requested to be encrypted
 	Data string	`json:"data"`
-}
-
-type EncryptResponse struct {
-	// base64 encoded string of encrypted data
-	Ciphertext string `json:"ciphertext"`
 }
 
 func encrypt(w http.ResponseWriter, req *http.Request) {
@@ -39,7 +39,7 @@ func encrypt(w http.ResponseWriter, req *http.Request) {
 	log.Printf("request body: %q\n", reader1)
 
 	// decode request
-	var encReq EncryptRequest
+	var encReq Payload
 	err := json.NewDecoder(req.Body).Decode(&encReq)
 	if err != nil {
 		http.Error(w, err.Error(), http.StatusBadRequest)
@@ -53,7 +53,7 @@ func encrypt(w http.ResponseWriter, req *http.Request) {
 	}
 
 	// generate random IV
-	nonce := make([]byte, 24)
+	nonce := make([]byte, NONCE_SIZE)
 	if _, err := rand.Read(nonce); err != nil {
 		http.Error(w, err.Error(), http.StatusInternalServerError)
 		return
@@ -71,26 +71,112 @@ func encrypt(w http.ResponseWriter, req *http.Request) {
 	// add nonce to end of ciphertext
 	plainBytes = append(plainBytes, nonce...)
 
-	response := EncryptResponse{Ciphertext: base64.StdEncoding.EncodeToString(plainBytes)}
-	log.Printf("response ciphertext: %s\n", response.Ciphertext)
+	response := Payload{Data: base64.StdEncoding.EncodeToString(plainBytes)}
+	log.Printf("b64 ciphertext: %s\n", response.Data)
 
 	w.Header().Set("Content-Type", "application/json")
-	json.NewEncoder(w).Encode(response)
+	err = json.NewEncoder(w).Encode(response)
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+}
 
+func decrypt(w http.ResponseWriter, req *http.Request) {
+	log.Printf("decrypt request from %s\n", req.Host)
+
+	// print request body
+	buf, bodyErr := ioutil.ReadAll(req.Body)
+	if bodyErr != nil {
+		http.Error(w, bodyErr.Error(), http.StatusInternalServerError)
+		return
+	}
+	reader1 := ioutil.NopCloser(bytes.NewBuffer(buf))
+	reader2 := ioutil.NopCloser(bytes.NewBuffer(buf))
+	req.Body = reader2
+
+	log.Printf("request body: %q\n", reader1)
+
+	// decode request
+	var decReq Payload
+	err := json.NewDecoder(req.Body).Decode(&decReq)
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusBadRequest)
+		return
+	}
+
+	// decode base64 data into bytes
+	ciphertext, err := base64.StdEncoding.DecodeString(decReq.Data)
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusBadRequest)
+		return
+	}
+
+	// separate into nonce and actual ciphertext
+	nonce := ciphertext[len(ciphertext) - NONCE_SIZE:]
+	ciphertext = ciphertext[:len(ciphertext) - NONCE_SIZE]
+
+	// decrypt data
+	cipher, err := chacha20.NewUnauthenticatedCipher(KEY, nonce)
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+	cipher.XORKeyStream(ciphertext, ciphertext)
+
+	plaintext := base64.StdEncoding.EncodeToString(ciphertext)
+	log.Printf("b64 plaintext: %s\n", plaintext)
+
+	w.Header().Set("Content-Type", "application/json")
+	err = json.NewEncoder(w).Encode(plaintext)
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
 }
 
 func init() {
 	log.SetFlags(log.Ldate | log.Lmicroseconds)
 
-	KEY = make([]byte, 32)
-	if _, err := rand.Read(KEY); err != nil {
-		panic(err)
+	redisClient = redis.NewClient(&redis.Options{
+		Addr:     "localhost:6379",
+		Password: "", // no password set
+		DB:       0,  // use default DB
+	})
+
+	if _, err := redisClient.Ping(ctx).Result(); err != nil {
+		log.Fatalf("error connecting to redis: %s\n", err.Error())
 	}
-	log.Printf("generated encryption key: %x\n", KEY)
+
+	encKey, err := redisClient.Get(ctx, "key").Result()
+	if err == redis.Nil {
+		log.Println("encryption key not found in redis")
+
+		KEY = make([]byte, 32)
+		if _, err := rand.Read(KEY); err != nil {
+			log.Fatalln(err)
+		}
+		log.Printf("generated encryption key: %x\n", KEY)
+
+		if err = redisClient.Set(ctx, "key", KEY, 0).Err(); err != nil {
+			log.Fatalf("error adding encryption key to redis: %s\n", err.Error())
+		} else {
+			log.Println("encryption key added to redis")
+		}
+	} else if err != nil {
+		log.Fatalf("error getting encryption key from redis: %s\n", err.Error())
+	} else {
+		KEY = make([]byte, 32)
+		KEY = []byte(encKey)
+
+		log.Printf("encryption key loaded from redis: %x\n", KEY)
+	}
 }
 
 func main() {
 	http.HandleFunc("/encrypt", encrypt)
+	http.HandleFunc("/decrypt", decrypt)
+
 
 	err := http.ListenAndServe(":8080", nil)
 	if err != nil {
